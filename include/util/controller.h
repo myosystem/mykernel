7a7645f9c5c46329b71d52185a2334afc966747c
corexaen <corexaen@gwnu.ac.kr>
2026-08-23 20:55:01 +0900

    upgrade: using new object

--- include/util/controller.h
+++ include/util/controller.h
@@ -1,23 +1,21 @@
 #ifndef __CONTROLLER_H__
 #define __CONTROLLER_H__
 #include "util/size.h"
+#include "util/new.h"
+#include "kernel/kernel.h"
 #define CONTROLLER_QUEUE_BASE 0xFFFF848000000000ULL
 #define CONTROLLERSTRUCT_SIZE 0x200
-class Controller {
+class Controller : public NewObject<pml4_addr(PML4::CONTROLLER_HEAP), CONTROLLERSTRUCT_SIZE, nullptr, nullptr> {
 protected:
     uint8_t pci_bus, pci_slot, pci_func;
 private:
-    uint64_t state;
     uint64_t controler_id;
 public:
     Controller(uint8_t bus, uint8_t slot, uint8_t func)
-        : pci_bus(bus), pci_slot(slot), pci_func(func) {
-    }
+        : pci_bus(bus), pci_slot(slot), pci_func(func) {}
     virtual ~Controller() {}
     virtual void init() = 0;
     virtual uint64_t get_type();
-    void* operator new(size_t size);
-    void operator delete(void* ptr);
     void (*on_disk_found)(class Disk* disk) = nullptr;
 };
 #endif // __CONTROLLER_H__
